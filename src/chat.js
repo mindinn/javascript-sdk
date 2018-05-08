608e6fc02fcd4d135e356e46932eaa089a68dfd5
Hazar Ilhan <hazarilhan@sabanciuniv.edu>
2018-05-08 19:03:59 +0300

    :sparkles: chat: Add update participant method
    # Conflicts:
    #	src/chat.js

--- src/chat.js
+++ src/chat.js
@@ -254,6 +254,48 @@ class Chat extends EventEmitterExtra {
   }
 
   /**
+   * @summary Update a participant of a room
+   *
+   * @param {!Number} payload.roomId Room id
+   * @param {!Number} payload.targetClientId Target client id
+   * @param {Boolean} payload.isAllowedToPost PostMessage priviledge of the participant
+   * @param {Object} payload.properties Additional properties for the participant
+   *
+   * @return {Promise}
+   *
+   * @example
+   *
+   * const socketkit = new SocketKit({
+   *   token: 'abc',
+   *   accountId: 1,
+   *   type: Socketkit.ConnectionType.CLIENT
+   * });
+   *
+   * socketkit.connect();
+   *
+   * socketkit.on(SocketKit.Event.CONNECTED, () => {
+   *   socketkit
+   *     .getInstance()
+   *     .updateParticipant({
+   *       roomId: 1,
+   *       targetUniqueClientKey: 15,
+   *       isAllowedToPost: true
+   *     });
+   * });
+   */
+  async updateParticipant({roomId, targetUniqueClientKey, isAllowedToPost, properties} = {}) {
+    if (!roomId)
+      return Promise.reject(new Error(`roomId is required`));
+
+    if (!targetUniqueClientKey)
+      return Promise.reject(new Error(`targetUniqueClientKey is required`));
+
+    const participant = {roomId, targetUniqueClientKey, isAllowedToPost, properties};
+
+    return this.client.send(Chat.InternalEvent.UPDATE_PARTICIPANT, participant);
+  }
+
+  /**
    * @summary Create a room
    *
    * @param  {!string} payload.title Title of the room
@@ -350,6 +392,7 @@ Chat.InternalEvent = {
   SEND_MESSAGE_TO_ROOM: 'send_message_to_room',
   CREATE_ROOM: 'create_room',
   ADD_PARTICIPANT: 'add_participant',
+  UPDATE_PARTICIPANT: 'update_participant',
   UPDATE_ROOM: 'update_room'
 };
 
